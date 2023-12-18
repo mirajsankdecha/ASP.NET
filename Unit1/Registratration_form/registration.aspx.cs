e7b0aa0ae752c26dc7e0590ec37bfbbb2119f495
mirajsankdecha <mirajsankdecha@gmail.com>
2023-12-18 12:49:05 +0530

    Registration from created

---
+++ Unit1/Registratration_form/registration.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Registratration_form
+{
+    public partial class registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
