883779d014a2a21b01a8aaf7acdef9b3599b2e3e
mirajsankdecha <mirajsankdecha@gmail.com>
2024-01-06 15:35:14 +0530

    Listbox Program created

---
+++ ASPDOTNET/Listbox .aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Listbox .aspx.cs" Inherits="Listbox_" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div align="center">
+        <div>
+            <asp:ListBox ID="ListBox1" runat="server" 
+                onselectedindexchanged="ListBox1_SelectedIndexChanged">
+                <asp:ListItem>Rajkot</asp:ListItem>
+                <asp:ListItem>Jetpur</asp:ListItem>
+                <asp:ListItem>Una</asp:ListItem>
+                <asp:ListItem>Ahemdabad</asp:ListItem>
+                <asp:ListItem>Surat</asp:ListItem>
+                <asp:ListItem>Dwarka</asp:ListItem>
+            </asp:ListBox>
+        </div>
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="btnadd" runat="server" Text="Add" onclick="Button1_Click" />
+            <asp:Button ID="btnrmv" runat="server" Text="Remove" onclick="Button2_Click" />
+        </div>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ ASPDOTNET/Listbox .aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Listbox_ : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        UpdateLabel();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        // Add the text from TextBox to the ListBox
+        if (!string.IsNullOrEmpty(TextBox1.Text))
+        {
+            ListBox1.Items.Add(new ListItem(TextBox1.Text));
+            TextBox1.Text = ""; // Clear the TextBox after adding
+            UpdateLabel();
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        // Remove selected items from the ListBox
+        for (int i = ListBox1.Items.Count - 1; i >= 0; i--)
+        {
+            if (ListBox1.Items[i].Selected)
+            {
+                ListBox1.Items.RemoveAt(i);
+            }
+        }
+
+        UpdateLabel();
+    }
+
+    private void UpdateLabel()
+    {
+        // Update the Label with the selected items in the ListBox
+        var str = "";
+        foreach (ListItem item in ListBox1.Items)
+        {
+            if (item.Selected)
+            {
+                str += "<li><b>" + item.Text + "</b></li>";
+            }
+        }
+        Label1.Text = str;
+    }
+}
