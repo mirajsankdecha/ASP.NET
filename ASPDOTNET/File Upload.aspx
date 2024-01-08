abc69d768b899714865b0ed18344484e9452997c
mirajsankdecha <mirajsankdecha@gmail.com>
2024-01-08 13:20:26 +0530

    File Upload Program update

--- ASPDOTNET/File Upload.aspx
+++ ASPDOTNET/File Upload.aspx
@@ -3,5 +3,16 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div align="center">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />  
+        </div>
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Button" onclick="Button1_Click" />
+        </div>
+        <div>
+            <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+        </div>
+    </div>
 </asp:Content>
 
--- ASPDOTNET/File Upload.aspx.cs
+++ ASPDOTNET/File Upload.aspx.cs
@@ -11,4 +11,28 @@ public partial class File_Upload : System.Web.UI.Page
     {
 
     }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile == null || FileUpload1.PostedFile.ContentLength > 0)
+        {
+            string path = System.IO.Path.GetExtension(FileUpload1.FileName);
+            if (path == ".png" || path == ".jpg" || path == ".jpeg" || path == ".jfif")
+            {
+                string location = Server.MapPath("files") + "\\" + FileUpload1.FileName;
+                FileUpload1.SaveAs(location);
+                Literal1.Text = "File Name: " + FileUpload1.FileName;
+                Literal1.Text += "<br>" + "File Type: " + FileUpload1.PostedFile.ContentType;
+                Literal1.Text += "<br>" + "File Size:" + FileUpload1.PostedFile.ContentLength;
+
+            }
+            else
+            {
+                Literal1.Text = "Please Select Image File Only!";
+            }
+        }
+        else
+        {
+            Literal1.Text = "Please Select Any File First!";
+        }
+    }
 }
